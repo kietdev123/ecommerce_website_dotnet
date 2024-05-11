a1268d3b27297bad6f99aa1148510dd8b18c0777
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-05-11 19:57:49 +0700

    Add project files.

---
+++ ecommerce_website_dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ecommerce_website_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
