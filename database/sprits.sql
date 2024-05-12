8f24728998f53bb813da6bf0cfc11af9305db14f
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-05-12 15:54:12 +0700

    chore: change server db

---
+++ database/sprits.sql
@@ -0,0 +1,6 @@
+use ecommerce_website
+
+backup database ecommerce_website
+to disk = 'C:\Users\Kiet\Desktop\kiet\my_project\ecommerce_website_dotnet\database\ecommerceWebsiteDB.bak'
+
+select * from AspNetUsers
