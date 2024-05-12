3347ffd5c2ce3f82d5c5b0119ac8af32ea55efc3
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-05-12 20:43:55 +0700

    chore: update connection string

--- database/sprits.sql
+++ database/sprits.sql
@@ -4,3 +4,13 @@ backup database ecommerce_website
 to disk = 'C:\Users\Kiet\Desktop\kiet\my_project\ecommerce_website_dotnet\database\ecommerceWebsiteDB.bak'
 
 select * from AspNetUsers
+
+select * from AspNetRoles
+
+select * from AspNetUserRoles
+
+INSERT INTO AspNetRoles
+values 
+('User','User','User','User'),
+('Admin','Admin','Admin','Admin');
+
